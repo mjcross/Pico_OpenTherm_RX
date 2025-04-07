c4c4e175418ee8765cd837addca100e89be92493
Martin Crossley <dev@crossleys.biz>
2025-04-07 22:17:49 +0100

    fix incorrect directory name and add transmitter test code

--- OT_rx.c
+++ OT_rx.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "OT_rx_library.h"
+#include "OT_tx_library.h"
 
 
 const PIO pio = pio0;
-const uint gpio = PICO_DEFAULT_LED_PIN;
+const uint gpio = 15;
 
 int main() {
     stdio_init_all();
+
+    uint tx_sm;
+    uint32_t tx_frame = 0x0fa1;
+    if (OT_tx_init (pio, &tx_sm, gpio)) {
+        puts ("tx code loaded");
+        while (true) {
+            sleep_ms (1000);
+            putchar('.');
+            pio_sm_put_blocking (pio, tx_sm, tx_frame);
+        }
+    } else {
+        puts ("problem loading tx code");
+        while (true);
+    }
+
 
     uint rx_sm;
     uint32_t rx_frame;
