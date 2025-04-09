683218043249158f5b798e168e80e3e80a4314b4
Martin Crossley <dev@crossleys.biz>
2025-04-09 13:18:43 +0100

    perform a loopback test: TX works but RX is unreliable

--- OT_rx.c
+++ OT_rx.c
@@ -10,35 +10,38 @@ const uint gpio = 15;
 int main() {
     stdio_init_all();
 
-    uint tx_sm;
-    uint32_t tx_frame = 0x0fa1;
+    uint tx_sm, rx_sm;
+    uint32_t tx_frame = 0x55555555UL, rx_frame;
+
     if (OT_tx_init (pio, &tx_sm, gpio)) {
-        puts ("tx code loaded");
-        while (true) {
-            sleep_ms (1000);
-            putchar('.');
-            pio_sm_put_blocking (pio, tx_sm, tx_frame);
-        }
+        puts ("tramsmitter running");
     } else {
         puts ("problem loading tx code");
         while (true);
     }
 
-
-    uint rx_sm;
-    uint32_t rx_frame;
     if (OT_rx_init (pio, &rx_sm, gpio)) {
-        puts ("running");
-        while(true) {
-            if (pio_sm_is_tx_fifo_empty (pio, rx_sm) == false) {
-                rx_frame = pio_sm_get_blocking (pio, rx_sm);
-                printf("RX: 0x%08lu\n", rx_frame);
-            }
- 
-            sleep_ms (1000);
-        }
+        puts ("receiver running");
+    } else {
+        puts ("problem loading rx code");
+        while (true);
     }
 
-    puts ("halted");
-    while (true);
+    while (true) {
+        sleep_ms (100);
+        printf("TX: 0x%032lb\t", tx_frame);
+        pio_sm_put_blocking (pio, tx_sm, tx_frame);
+
+        sleep_ms (1000);
+        printf ("RX: ");
+        if (pio_sm_is_rx_fifo_empty (pio, rx_sm)) {
+            putchar ('-');
+        } else {
+            while (pio_sm_is_rx_fifo_empty (pio, rx_sm) == false) {
+                rx_frame = pio_sm_get_blocking (pio, rx_sm);
+                printf ("0x%032lb ", rx_frame);
+            }
+        }
+        putchar ('\n');
+    }
 }
